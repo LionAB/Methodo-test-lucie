e463feb996d74c7132b6cf5b7d07515074657f52
LionAB <lionelabatan@gmail.com>
2024-06-25 15:24:27 +0200

    stringCalculator6 return false sum when contain /n

--- src/tests/calculator.test.ts
+++ src/tests/calculator.test.ts
@@ -13,4 +13,7 @@ describe('String Calculator', () => {
     it('should return the sum of the string or numbers when they are separated by a coma', () => {
         expect(stringCalculator('2, 2')).toBe(4);
     });
+    it("should return the sum of the numbers when they conatains '\n'", () => {
+        expect(stringCalculator('1\n2,3')).toBe(6);
+    });
 });
