afb649bfd7cc3fee4ef35a3a5d7b250a2dcbfba9
LionAB <lionelabatan@gmail.com>
2024-06-25 15:12:34 +0200

    stringCalculator4 1.2 SUCCESS for trying to sum the numbers separated by a coma

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,5 +2,8 @@ export  const stringCalculator = (input: string) : number  => {
     if (input === '') {
         return 0;
     }
+    if (input.includes(',')) {
+        return input.split(',').reduce((acc, curr) => acc + Number(curr), 0);
+    }
  return Number(input);
 }
