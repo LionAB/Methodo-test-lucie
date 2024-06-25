ac7f4116295fd0e6780317f75523c038bf3a9135
LionAB <lionelabatan@gmail.com>
2024-06-25 14:10:09 +0200

    FizzBuzz 3 - test OK  return Fizz when the parameter is divisible by 5+ refactor

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ export function fizzBuzz (number: number): string {
     if (number % 3 === 0) {
         return 'Fizz';
     }
+    if (number % 5 === 0) {
+        return 'Buzz';
+    }
     else return "";
 }
