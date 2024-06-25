249dc4f5d12d9b3aef8fe3bc76caece0927cef06
LionAB <lionelabatan@gmail.com>
2024-06-25 14:20:36 +0200

    FizzBuzz 5 - test OK  return NOPE ! when the parameter is not  divisible by 5 or 3+ refactor + fix  3 and 5

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 export function fizzBuzz (number: number): string {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz';
+    }
     if (number % 3 === 0) {
         return 'Fizz';
     }
     if (number % 5 === 0) {
         return 'Buzz';
     }
-    if (number % 3 === 0 && number % 5 === 0) {
-        return 'FizzBuzz';
-    }
-    else return "";
+   
+    else return "NOPE !";
 }
