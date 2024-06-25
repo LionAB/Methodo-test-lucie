fff6b21b53ee9315a78217408f0abf17869bc8cb
LionAB <lionelabatan@gmail.com>
2024-06-25 15:28:24 +0200

    stringCalculator3 SUCCESS for trying to sum the numbers separated by a com

--- src/calculator.ts
+++ src/calculator.ts
@@ -2,7 +2,10 @@ export  const stringCalculator = (input: string) : number  => {
     if (input === '') {
         return 0;
     }
-    if (input.includes(',')) {
+    if (input.includes(',') || input.includes('\n')) {
+        if (input.includes('\n')) {
+            input = input.replace('\n', ',');
+        }
         return input.split(',').reduce((acc, curr) => acc + Number(curr), 0);
     }
  return Number(input);
